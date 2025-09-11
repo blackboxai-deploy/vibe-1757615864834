d3737b394d7ce5c4dec9e5902c615c8bb30fca87
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 00:08:33 +0530

    Add src/components/leads-list.tsx

---
+++ src/components/leads-list.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+export function LeadsList() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Leads List</CardTitle>
+        <CardDescription>Manage and organize your leads</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Leads management interface coming soon...</p>
+      </CardContent>
+    </Card>
+  )
+}
