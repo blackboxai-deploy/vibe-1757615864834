a8c455c44e48721e7ec0c5d90af59656105f8b4b
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 00:08:37 +0530

    Add src/components/lead-enrichment.tsx

---
+++ src/components/lead-enrichment.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+export function LeadEnrichment() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Lead Enrichment</CardTitle>
+        <CardDescription>Enhance lead profiles with additional data</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Lead enrichment features coming soon...</p>
+      </CardContent>
+    </Card>
+  )
+}
