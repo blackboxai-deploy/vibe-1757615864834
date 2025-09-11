ea6e811df1e88557cb930f355b02a7df3284b107
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 00:08:38 +0530

    Add src/components/export-history.tsx

---
+++ src/components/export-history.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+export function ExportHistory() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Export History</CardTitle>
+        <CardDescription>View past data exports and downloads</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Export history coming soon...</p>
+      </CardContent>
+    </Card>
+  )
+}
