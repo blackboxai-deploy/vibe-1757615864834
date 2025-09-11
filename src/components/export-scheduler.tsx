2dde5b5f9da4c71ade2c697086de9e6999fee558
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 00:08:38 +0530

    Add src/components/export-scheduler.tsx

---
+++ src/components/export-scheduler.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+export function ExportScheduler() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Export Scheduler</CardTitle>
+        <CardDescription>Schedule automated data exports</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Export scheduler coming soon...</p>
+      </CardContent>
+    </Card>
+  )
+}
