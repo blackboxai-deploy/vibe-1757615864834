298de8c84a317be1b39fe662987f04b24b169d32
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 00:08:42 +0530

    Add src/components/webhook-settings.tsx

---
+++ src/components/webhook-settings.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+export function WebhookSettings() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Webhook Settings</CardTitle>
+        <CardDescription>Configure webhooks and event notifications</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Webhook settings coming soon...</p>
+      </CardContent>
+    </Card>
+  )
+}
