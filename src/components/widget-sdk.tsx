6347e5905f5c3bba8933424a37a467feb5a91241
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 00:08:34 +0530

    Add src/components/widget-sdk.tsx

---
+++ src/components/widget-sdk.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+export function WidgetSDK() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Widget SDK</CardTitle>
+        <CardDescription>API documentation and SDK resources</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>SDK documentation coming soon...</p>
+      </CardContent>
+    </Card>
+  )
+}
