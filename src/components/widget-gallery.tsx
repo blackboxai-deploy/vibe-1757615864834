465a72d17ffb492cafc3305ba05c17ac0b40c355
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 00:08:34 +0530

    Add src/components/widget-gallery.tsx

---
+++ src/components/widget-gallery.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+export function WidgetGallery() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Widget Gallery</CardTitle>
+        <CardDescription>Browse pre-built widget templates</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Widget gallery coming soon...</p>
+      </CardContent>
+    </Card>
+  )
+}
