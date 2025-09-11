b278cd8a092aa252bb2712e763495d92cf4c2f65
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 00:08:32 +0530

    Add src/components/bot-analytics.tsx

---
+++ src/components/bot-analytics.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+export function BotAnalytics() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Bot Analytics</CardTitle>
+        <CardDescription>Performance analytics and insights</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Bot analytics dashboard coming soon...</p>
+      </CardContent>
+    </Card>
+  )
+}
