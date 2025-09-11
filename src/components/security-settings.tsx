1362ff570cf84e539cacd5372cea25313d25f5b7
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 00:08:39 +0530

    Add src/components/security-settings.tsx

---
+++ src/components/security-settings.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+export function SecuritySettings() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Security Settings</CardTitle>
+        <CardDescription>Configure security and access controls</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Security settings coming soon...</p>
+      </CardContent>
+    </Card>
+  )
+}
