c0e3836cfb31ab68c71572edbd83126407f0ed96
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 00:08:40 +0530

    Add src/components/performance-analytics.tsx

---
+++ src/components/performance-analytics.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+export function PerformanceAnalytics() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Performance Analytics</CardTitle>
+        <CardDescription>Bot performance and system metrics</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Performance analytics coming soon...</p>
+      </CardContent>
+    </Card>
+  )
+}
