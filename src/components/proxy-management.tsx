73ccc4b3a33bfa4d4b232a26dbb05ed03015ba52
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 00:08:39 +0530

    Add src/components/proxy-management.tsx

---
+++ src/components/proxy-management.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+export function ProxyManagement() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Proxy Management</CardTitle>
+        <CardDescription>Configure proxy pools and rotation settings</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Proxy management interface coming soon...</p>
+      </CardContent>
+    </Card>
+  )
+}
